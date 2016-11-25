09b8daeba9afe1c92a19a5233003d021f0eb8fec
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2016-11-25 23:20:58 +0300

    microfix

--- js/slides/rulesElement.js
+++ js/slides/rulesElement.js
@@ -9,7 +9,7 @@ const formElement = rulesElement.querySelector('.rules__form');
 const inputElement = rulesElement.querySelector('.rules__input');
 const submitElement = rulesElement.querySelector('.rules__button');
 
-if (true || formElement && inputElement && submitElement) {
+if (formElement && inputElement && submitElement) {
 
   const onInput = () => {
     submitElement.disabled = inputElement.value.length === 0;
