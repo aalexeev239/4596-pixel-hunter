e4621cce675104fbe71cf5617a2374c61a1544cc
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2016-11-28 21:31:10 +0300

    microfix

--- js/templates/renderGameHeader.js
+++ js/templates/renderGameHeader.js
@@ -3,7 +3,7 @@ import headerBackTemplate from './headerBack';
 const func = ({time, lives: {left, total}}) => {
   const diff = total - left;
 
-  const heartsArrayTemplate = [...Array(total)].map((live, i) => `
+  const heartsArrayTemplate = [...Array(total)].map((life, i) => `
   <img src="img/heart__${i < diff ? 'empty' : 'full'}.svg" 
   class="game__heart" alt="Life" width="32" height="32">`);
 
