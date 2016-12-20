440590ab931541a07abac3f7e926b33b305789fa
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2016-12-20 08:28:00 +0300

    module4-task2 | fix styles

--- js/templates/renderOptionSelect.js
+++ js/templates/renderOptionSelect.js
@@ -1,6 +1,6 @@
 const func = ({image: {url, title, width, height}, isSelected}) => `<label class="game__option${isSelected ? ' game__option--selected' : ''}">
     <img src="${url}" alt="${title}" width="${width}" height="${height}">
-    <input type="radio" name="question" value="${title}" name="${title}">
+     <input type="radio" name="question" value="${title}" name="${title}" class="hidden">
   </label>`;
 
 export default func;
