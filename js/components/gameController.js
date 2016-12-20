9e6f33b27230d1b0b2e660709a1b64b54572f7bf
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2016-12-21 00:05:00 +0300

    module4-task2 | microfix

--- js/components/gameController.js
+++ js/components/gameController.js
@@ -5,11 +5,10 @@ import {setAnswer, setQuestion, getStatsData} from './game';
 import {questions} from '../data/game-data';
 
 export const gameController = (state) => {
-  state = setQuestion(state, state.currentQuestion);
   renderSlide(getGameElement(state, (answer) => {
     state = setAnswer(state, answer);
     if (state.lives > 0 && state.currentQuestion < questions.length - 1) {
-      state.currentQuestion = state.currentQuestion + 1;
+      state = setQuestion(state, state.currentQuestion + 1);
       gameController(state);
     } else {
       renderSlide(getStatsElement(getStatsData(state)));
