aa7c91efb9c234f9b7cd1dcc1073b492d55453ae
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2016-12-22 09:33:01 +0300

    module5-task1 | microfix

--- js/screens/rules.js
+++ js/screens/rules.js
@@ -62,7 +62,7 @@ class IntroScreen extends AbstractView {
     ev.preventDefault();
 
     if (this._checkInputValidity()) {
-      console.log('--- wooo', wooo);
+      console.log('--- wooo');
     }
   };
 }
